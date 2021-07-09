47e7cff953bc3152c7cb3c41ffb4052b8676a057
Master_Hans <suvorov.ag.75@gmail.com>
2021-07-09 16:19:55 +0300

    Init - installed YY2-basic

---
+++ views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Геттеры и Сеттеры';
+?>
+<div class="site-index">
+    <div class="jumbotron">
+        <h1>Геттеры и Сеттеры: на примере в PHP, а также в YII2</h1>
+    </div>
+</div>
